d8cbe3d549e9ac5a88c9938808e3f8f82f47bf94
Jack Cameron <jackmcameron01@gmail.com>
2018-08-11 14:10:00 -0400

    Initial Commit

---
+++ GitXC9/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  GitXC9
+//
+//  Created by Jack Cameron on 8/11/18.
+//  Copyright © 2018 Jack Cameron. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ GitXC9/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  GitXC9
+//
+//  Created by Jack Cameron on 8/11/18.
+//  Copyright © 2018 Jack Cameron. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
